9a0c8c81791ce5e3ef449fe7e3a680d84aa80e3c
Mikhail Kozlov <kozlov.m.yu@yandex.ru>
2017-02-25 06:02:26 +0300

    My solutions from February 25th

---
+++ src/com/javarush/lesson4/task44/PositiveAndNegativeNum.java
@@ -0,0 +1,27 @@
+package com.javarush.lesson4.task44;
+
+import java.util.Scanner;
+
+/**
+ * Created by Arsen on 25.02.2017.
+ * Ввести с клавиатуры число.
+ Если число положительное, то увеличить его в два раза.
+ Если число отрицательное, то прибавить единицу.
+ Если введенное число равно нулю, необходимо вывести ноль.
+ Вывести результат на экран.
+ */
+public class PositiveAndNegativeNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a > 0){
+            a*=2;
+            System.out.println(a);
+        } else if (a < 0){
+            a++;
+            System.out.println(a);
+        } else{
+            System.out.println(a);
+        }
+    }
+}
